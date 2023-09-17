a40f89591319393af7f6ffa49c1d886545d1179d
Martin Spinler <martin.spinler@gmail.com>
2023-09-17 18:04:26 +0200

    hw bridge: throttle/discard some frequent CC updates on slow BLE
    This should fix the input MIDI buffer overflow
    and be malfunctional till reset.

--- hw-xiao-nrf52840-bridge/midi.cpp
+++ hw-xiao-nrf52840-bridge/midi.cpp
@@ -9,6 +9,29 @@ const uint8_t MIDIBOX_SYSEX_ID1 = 0x77;
 const uint8_t MIDIBOX_SYSEX_ID2 = 0x78;
 const uint8_t MIDIBOX_SYSEX_PEDALS = 0x79;
 
+#define THROTTLE_CC_SIZE 8
+const uint8_t throttle_cc[THROTTLE_CC_SIZE] = {
+	Sustain,
+	Portamento,
+	Sostenuto,
+	SoftPedal,
+	Effects1,
+	Effects3,
+	ChannelVolume,
+	ExpressionController,
+};
+
+struct throttle {
+	unsigned long lastsync;
+	uint8_t lastval;
+	uint8_t currval;
+};
+
+#define THROTTLE_CC_CHANNEL_SIZE 3
+struct throttle throttle[THROTTLE_CC_CHANNEL_SIZE][THROTTLE_CC_SIZE];
+
+#define THROTTLE_CC_TIME 40
+
 void midi_handle_pedal_input(uint8_t pedal, uint8_t val)
 {
 	uint8_t i;
@@ -39,8 +62,36 @@ void handleBleMidiMessage(const Message<128> & msg)
 
 void handleS1MidiMessage(const midi::Message<128> & msg)
 {
-	MB.send(msg);
+	static uint8_t i;
+	static uint8_t filter;
+	static unsigned long ct;
+
+	ct = millis();
+
 	MU.send(msg);
+
+	if (msg.type == ControlChange &&
+			msg.channel <= THROTTLE_CC_CHANNEL_SIZE) {
+		filter = 0;
+		for (i = 0; i < THROTTLE_CC_SIZE; i++) {
+			if (msg.data1 == throttle_cc[i]) {
+				struct throttle & t = throttle[msg.channel-1][i];
+
+				t.currval = msg.data2;
+				if (t.lastsync + THROTTLE_CC_TIME < ct) {
+					filter = 1;
+				} else {
+					t.lastval = t.currval;
+					t.lastsync = ct;
+				}
+			}
+		}
+		if (!filter) {
+			MB.send(msg);
+		}
+	} else {
+		MB.send(msg);
+	}
 }
 
 void midi_init()
@@ -60,7 +111,26 @@ void midi_init()
 
 void midi_loop()
 {
+	static uint8_t i;
+	static uint8_t c;
+	static unsigned long ct;
+
 	MU.read();
 	MB.read();
 	MS1.read();
+
+	ct = millis();
+
+	for (c = 0; c < THROTTLE_CC_CHANNEL_SIZE; c++) {
+		for (i = 0; i < THROTTLE_CC_SIZE; i++) {
+			struct throttle & t = throttle[c][i];
+			if (t.currval != t.lastval) {
+				if (t.lastsync + THROTTLE_CC_TIME >= ct) {
+					t.lastval = t.currval;
+					t.lastsync = ct;
+					MB.sendControlChange(throttle_cc[i], t.currval, c+1);
+				}
+			}
+		}
+	}
 }
