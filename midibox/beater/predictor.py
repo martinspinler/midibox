9276a232f0103fbbdd52d1d9a7e7214a6cf3411a
Martin Spinler <martin.spinler@gmail.com>
2025-11-12 19:48:48 +0100

    chore(beater): clean code

--- midibox/beater/predictor.py
+++ midibox/beater/predictor.py
@@ -1,26 +1,30 @@
 from dataclasses import dataclass, field
 from typing import Optional, Any
+import math
 import logging
 import numpy as np
 
 
 log_phaser = logging.getLogger("midibox.beater.phaser")
 
+
 def pfl(fl):
     return "[" + ", ".join([f"{n:.3f}" for n in fl]) +"]"
 
 
-def to_interval(val, inc, to, fr=0):
-    cnt = 0
+def to_interval(val: float, inc: float, to: float, fr: float = 0):
+    if fr <= val <= to:
+        return val, 0
+
     if val > to:
-        while val - inc >= fr:
-            val -= inc
-            cnt += 1
+        steps = int((val - fr) // inc)
+        new_val = val - steps * inc
+        steps = -steps
     else:
-        while val < fr:
-            val += inc
-            cnt -= 1
-    return val, cnt
+        steps = math.ceil((fr - val) / inc)
+    new_val = val + steps * inc
+
+    return new_val, -steps
 
 
 Event = type[float]
@@ -63,15 +67,12 @@ class Prediction:
 
 class TempoPredictor:
     def __init__(self, visualiser, debug=False):
-        self.beat_len = 0
-        self.beats_abs = []
         self.events = []
+        self.beats_abs = []
         self.beat_last = None
+        self.beat_len = 0
         self.beat_len_hint = None
-        self.beat_len_avg = []
         self.v = visualiser
-        self.offbeats = []
-        self.events_in_process = []
 
         self.beat_len_predicted = 1e9
         self.current_prediction = Prediction()
@@ -88,8 +89,6 @@ class TempoPredictor:
 
     def reset(self, time):
         notes = self.events
-        self.beat_len_avg = []
-        self.offbeats = []
 
         self.beat_len = 0
         self.beat_last = time
@@ -111,26 +110,25 @@ class TempoPredictor:
                 self.beat_len = self.beat_len_hint
 
         self.beat_last = notes[0]
-        self.beats_abs = [notes[0], notes[1]]
         self.beats_abs = [notes[0]]
         return beat_len
 
     def on_note_event(self, time, msg_id, msg, msg_text):
-        current = time
+        event = time
         note = msg.note
 
         if len(self.events):
-            abs_diff = current - self.events[-1]
+            abs_diff = event - self.events[0]
             if abs_diff / self._time_mult > 7:  # 7 seconds (real seconds)
                 self.events.clear()
 
         self.check_beat(time)
-        self.events.append(time)
+        self.events.insert(0, time)
         self.v.notes.append([time, note])
 
         if len(self.events) == 1:
             self.reset(time)
-            self.beat(0) #self.beat_len)
+            self.beat(0)
         elif len(self.events) == 2:
             self.prepare_first_beat()
             self.beat(self.beat_len)
@@ -138,36 +136,23 @@ class TempoPredictor:
         if self._debug:
             print("=" * 10, f"{msg_id: 4d} {msg_text}", f"{time:.3f}")
 
-        if True or len(self.events) > 2:
-            self.offbeats.insert(0, current)
-        self.events_in_process.append(current)
-
         self.current_prediction = self.predict()
 
         if self.current_prediction.beat_len is not None:
             self.beat_len_predicted = self.current_prediction.beat_len
 
     def predict_note(self, e, beat_exp, beat_len, beat_phase):
-        # Max divergence to be a beat
-        MAX_DIV = 0.20
         # ed:   event difference
         # edi:  event difference in interval (0, beat_len)
         # edn:  event difference normalized (percentual value to beat)
         # edni: event difference normalized in interval (0, 1), 1 means one beat ago
-        #ed = beat_exp - e
         ed = e - beat_exp
-        #ed = e - self.beat_last
         edi, cnt = to_interval(ed, beat_len, beat_len, 0)
         edn = ed / beat_len
         edni, cnt = to_interval(edn, 1, 1, 0)
         acnt = -cnt if cnt < 0 else 1
 
         # HERE sort to 1/3, 2/3, 1/2 ....
-        probability = 0
-        weight = 0
-        weight_pow = 0
-        timestamp = None
-
         if edni <= 0.5:
             timestamp = edi + 1
             probability = 1 - edni * 2
@@ -177,31 +162,33 @@ class TempoPredictor:
             probability = edni * 2 - 1
             st = ">"
 
-        weight = probability / (acnt*3.5)
+        weight = probability / (acnt * 3.5)
         weight_pow = probability * probability / (acnt * acnt)
-        state = f"{st} {timestamp:.3f} | {1/acnt:.3f} = {probability:.3f}, {weight:.3f}, {weight_pow:.3f}"
 
         p = NotePrediction(e, timestamp, probability, weight, weight_pow, acnt)
         if self._debug:
+            state = f"{st} {timestamp:.3f} | {1/acnt:.3f} = {probability:.3f}, {weight:.3f}, {weight_pow:.3f}"
             p.dbg_state = (f"{e:.3f} {beat_phase:.2f} | {ed: 5.3f} {edi: 5.3f} {edn: 5.3f} {edni: 5.3f} | {cnt: 3d} {acnt} {state}")
         return p
         
     def predict_notes(self, probed_notes, beat_phase):
         all_best = []
-        for e in probed_notes:
+        unused_index = None
+        for index, e in enumerate(probed_notes):
             best = None
             all_local = []
             for local_beat_phase in [0, 1/3, 2/3, 1/2]:
             #for local_beat_phase in [0, 1/2]:
                 beat_exp = self.beat_last + (self.beat_len * (beat_phase + local_beat_phase))
                 n_prediction = self.predict_note(e, beat_exp, self.beat_len, local_beat_phase)
-                all_local.append((n_prediction, local_beat_phase))
+
+                if self._debug:
+                    all_local.append((n_prediction, local_beat_phase))
 
                 if best is None or best.p.probability < n_prediction.probability:
                     best = NoteOffsetPrediction(n_prediction, local_beat_phase, [])
 
-            if self._debug:
-                best.dbg_all_p = all_local
+            best.dbg_all_p = all_local
 
             if best.p.probability < 0.55:
                 if self._debug:
@@ -213,8 +200,10 @@ class TempoPredictor:
                 all_best.append(best)
 
             if best.p.rel_beat > 32:
+                unused_index = index
                 break
-        return all_best
+
+        return all_best, unused_index
 
     def predict(self):
         if self.beat_len == 0 or len(self.beats_abs) < 2:
@@ -230,10 +219,12 @@ class TempoPredictor:
         #for beat_phase in [0]: # 0 is implicit (from previous line)
         all_phases = []
         for beat_phase in [0, 1/3, 2/3, 1/2, 1/4]:
-            predicted_notes = self.predict_notes(self.offbeats, beat_phase)
+            predicted_notes, unused_index = self.predict_notes(self.events, beat_phase)
+            if unused_index is not None:
+                del self.events[unused_index:]
 
             predicted_notes_sel = [(i.p.rel_time, i.p.weight, i.p.weight_pow) for i in predicted_notes]
-            notes, notes_weight, notes_weight_pow = [list(i) for i in zip(*predicted_notes_sel)]
+            notes, notes_weight, notes_weight_pow = [list(i) for i in zip(*predicted_notes_sel)] if len(predicted_notes_sel) else ([], [], [])
 
             events, events_weight = ((beats + notes), (beats_weight + notes_weight))
             if len(events) < 1:
@@ -303,7 +294,7 @@ class TempoPredictor:
                 #print(f"TPRED: {pl.beat_len:.3f} | NTP: {sum(notes_weight) / len(notes_weight)} | {pfl(beats[-3:]):35s} {pfl(beats_weight[-3:]):35s} {pfl(notes):35s} {pfl(notes_weight):35s}")
                 print(f"TPRED: {p.beat_len:.3f} | NTP: {sum(notes_weight) / len(notes_weight)} | {pfl(beats[-3:]):35s} {pfl(beats_weight[-3:]):35s}")
 
-            for nop in reversed(p.dbg_predicted_notes):
+            for nop in p.dbg_predicted_notes:
                 if nop.p.event > self._dbg_last_showed_event:
                     #nop.p.event
                     self.v.predictions.append([nop.p.event, nop.beat_phase, nop.p.probability])
