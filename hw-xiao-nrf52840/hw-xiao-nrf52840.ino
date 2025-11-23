29efe8ae99bb302149bf371ff72013cf02e87458
Martin Spinler <martin.spinler@gmail.com>
2025-11-23 21:25:45 +0100

    feat(hw): set default pedal min/max values for hw-xiao-nrf52840

--- hw-xiao-nrf52840/hw-xiao-nrf52840.ino
+++ hw-xiao-nrf52840/hw-xiao-nrf52840.ino
@@ -199,6 +199,13 @@ void setup()
 	midi_init();
 	smidi_init();
 
+	gs.r.pedal_min[0] = 80;
+	gs.r.pedal_max[0] = 100;
+	gs.r.pedal_min[1] = 10;
+	gs.r.pedal_max[1] = 11;
+	gs.r.pedal_min[2] = 8;
+	gs.r.pedal_max[2] = 110;
+
 #ifdef MIDIBOX_ENABLE_BLE
 	ble_start_adv();
 #endif
